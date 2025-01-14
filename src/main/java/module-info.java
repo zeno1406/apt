0ed037632609a5afde62b3c5a1d252c6e6cff10d
Zeno OOP <ngthanhlong2003@gmail.com>
2025-01-14 13:21:10 +0700

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.apt {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.apt to javafx.fxml;
+    exports com.example.apt;
+}
