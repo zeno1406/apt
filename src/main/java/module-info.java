127b498e6a5232034b7bbfbff5a8ec42144f1adc
Thien_Lang <kieutantai2x@gmail.com>
2025-03-17 16:09:20 +0700

    temp

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,12 @@
-module com.example.apt {
+module com.example.shop {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires eu.hansolo.tilesfx;
+    requires org.kordamp.bootstrapfx.core;
 
-    opens com.example.apt to javafx.fxml;
-    exports com.example.apt;
+    opens com.example.shop to javafx.fxml;
+    exports com.example.shop;
 }
