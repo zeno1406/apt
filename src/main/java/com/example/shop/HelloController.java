70ca05d3414f44e88752d3cbb905af44f0a17c6c
Thien_Lang <kieutantai2x@gmail.com>
2025-03-26 23:47:08 +0700

    add form login

--- src/main/java/com/example/shop/HelloController.java
+++ src/main/java/com/example/shop/HelloController.java
@@ -1,14 +1,51 @@
 package com.example.shop;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button btnAccount;
+    @FXML
+    private TextField txtSearchProduct;
+    @FXML
+    private Pane pMainContentPane;
+    @FXML
+    private VBox vbMainMenu;
+    @FXML
+    private AnchorPane acTopMainMenu;
+    @FXML
+    private Pane pFormLoginRegis;
+    @FXML
+    private Button btnExitAccountForm;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onBtnAccountClicked(MouseEvent event) {
+        txtSearchProduct.setText("Welcome to JavaFX Application!");
+        pFormLoginRegis.setVisible(true);
+
+    }
+
+    @FXML
+    protected void onBtnExitAccountFormClicked(MouseEvent event) {
+        pFormLoginRegis.setVisible(false);
+        txtSearchProduct.setText("Hide");
+    }
+
+    public void initialize() {
+        double parentWidth = ((AnchorPane) pMainContentPane.getParent()).getPrefWidth();
+        double parentHeight = ((AnchorPane) pMainContentPane.getParent()).getPrefHeight();
+        double vBoxMainMenuWidth = vbMainMenu.getPrefWidth();
+        double acTopMainMenuHeight = acTopMainMenu.getPrefHeight();
+        pMainContentPane.setPrefWidth(parentWidth - vBoxMainMenuWidth);
+        pMainContentPane.setPrefHeight(parentHeight - acTopMainMenuHeight);
+        txtSearchProduct.setText(String.valueOf(parentWidth));
+
     }
 }
