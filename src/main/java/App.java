d1a89d9ac4c46fbd3c09656ab5c0b58dae887574
ThanThoaiBacAu Developer <ngthanhlong2003@gmail.com>
2025-04-14 12:47:13 +0700

    modal ui category supplier

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -38,7 +38,7 @@ public class App extends Application {
 
 
     public static void main(String[] args) {
-//        System.out.println(PasswordUtils.getInstance().hashPassword("TLANG1234"));
+//        System.out.println(PasswordUtils.getInstance().hashPassword("123456"));
         launch(args);
     }
 }
